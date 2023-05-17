af899fd94d2529c5f8002d1b6889f86d398b0aad
Harsh <harshrawat096@gmail.com>
2023-05-17 07:15:33 +0530

    first commit

---
+++ Dog Api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import httpx
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def home(request: Request):
+    url = "https://dog.ceo/api/breeds/image/random"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        dog_data = response.json()
+    return templates.TemplateResponse("index.html", {"request": request, "dog_image": dog_data["message"]})
